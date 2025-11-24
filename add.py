d12c010482df369e2d4c72ea699865c51806ea36
PeacefulAndTranquil <128899926+PeacefulAndTranquil@users.noreply.github.com>
2025-11-24 11:33:59 +0000

    fix test not work (real) (working 2025)

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 def add(x: int, y: int) -> int:
     """Returns the sum of two integers."""
-    if type(x) is not int or type (y) is not int:
+    if type(x) is not int or type(y) is not int:
         raise TypeError
     return x + y
